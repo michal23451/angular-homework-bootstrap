d54312de79ee9deab06c88470b14d6fe841eabc4
Michał Konowalski <konowalski.michal@gmail.com>
2022-08-26 02:03:35 +0200

    homework bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarMenuComponent } from './car-menu/car-menu.component';
+import { CarTableComponent } from './car-table/car-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarMenuComponent,
+    CarTableComponent
   ],
   imports: [
     BrowserModule
